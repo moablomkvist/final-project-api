be00902c5aea74ced5a0bf2b38b042f43ff6aff7
Moa Blomkvist <moa@birgerochblom.se>
2021-01-25 22:45:32 +0100

    mongoose model and first endpoints

--- server.js
+++ server.js
@@ -1,24 +1,79 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/knitting-circle";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+const Pattern = mongoose.model("Pattern", {
+  post: {
+    type: String,
+    difficulty: Number,
+    required: true //consider a maxlength to controll the size of feed.
+  },
+  source: {
+    type: String, 
+    required: true
+  },
+  needles: {
+    type: Number
+  },
+  yarn: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  likes: {
+    type: Number,
+    default: 0
+  },
+  comments: [{ body: String, date: Date }],
+});
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+const port = process.env.PORT || 8080;
+const app = express();
 
-// Start the server
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello knitting world");
+});
+
+app.get("/patterns", async (req, res) => {
+
+try {
+  const patterns = await Pattern.find()
+    .sort({ createdAt: "desc" })
+    .limit(24)
+    .exec();
+  res.json(patterns);
+} catch (err) {
+  res.status(400).json({ message: "could not load the database" });
+}
+});
+
+app.post("/patterns", async (req, res) => {
+  const { post, source, needles, yarn, createdAt, likes, comments } = req.body;
+  const pattern = new Pattern({ post: post, source: source, needles: needles, yarn: yarn, createdAt: createdAt, likes: likes, comments: comments });
+
+  try {
+    const savedPattern = await pattern.save();
+    res.status(201).json(savedPattern);
+  } catch (err) {
+    res
+      .status(400)
+      .json({
+        message: "Could not post your pattern",
+        error: err.errors,
+      });
+  }
+});
+
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
